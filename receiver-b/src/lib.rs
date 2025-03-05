562fc9910f58a2cab72a585c5b4571cf79cba737
jaxs-ribs <luc.baracat@googlemail.com>
2025-03-05 16:14:19 -0500

    Add some downstream enums and structs

--- receiver-b/src/lib.rs
+++ receiver-b/src/lib.rs
@@ -1,8 +1,8 @@
 #![allow(warnings)] // TODO: Zena: Remove this and fix warnings
 use hyperprocess_macro::hyperprocess;
+use hyperware_process_lib::http::server::WsMessageType;
 use hyperware_process_lib::logging::info;
 use hyperware_process_lib::LazyLoadBlob;
-use hyperware_process_lib::http::server::WsMessageType;
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Serialize, Deserialize)]
@@ -26,9 +26,26 @@ impl ReceiverBState {
     }
 
     #[local]
-    fn hello(&mut self, value: String) -> f32 {
-        let num_chars = value.len() as f32;
+    fn hello(&mut self, struct_val: SomeStruct) -> f32 {
+        let num_chars = struct_val.field1.len() as f32;
+        let num_chars_enum = match struct_val.field3 {
+            SomeEnum::Variant1(s) => s.len() as f32,
+            SomeEnum::Variant2(i) => i as f32,
+        };
         info!("Received string of length {}", num_chars);
         num_chars
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum SomeEnum {
+    Variant1(String),
+    Variant2(i32),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SomeStruct {
+    field1: String,
+    field2: i32,
+    field3: SomeEnum,
 }
